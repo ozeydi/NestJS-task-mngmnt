d8d113d335be271ddac33337850748762408276c
ozeydi <zeidiwassa@gmail.com>
2026-03-30 12:24:58 +0200

    feat: add task creation with uuid and status enum

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { Task, TaskStatus } from './task.model';
+import { TasksModule } from './tasks.module';
 
 @Injectable()
 export class TasksService {
-  private tasks = []; //array of tasks
-  getAllTasks() {
+  private tasks: Task[] = []; //array of tasks
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    console.log(this.tasks);
+    return task;
   }
 }
