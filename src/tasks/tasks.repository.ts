7574554b63dfe7ed5f16e3698a1661b6b376f116
ozeydi <zeidiwassa@gmail.com>
2026-04-28 09:29:33 +0200

    feat: implement filtering functionality for getTasks method in TasksController and TasksService

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -4,11 +4,30 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksRepository extends Repository<Task> {
   constructor(@InjectDataSource() dataSource: DataSource) {
     super(Task, dataSource.createEntityManager());
+  }
+
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.createQueryBuilder('task');
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+    if (search) {
+      query.andWhere(
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    console.log(filterDto);
+    return tasks;
   }
 
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,10 +4,15 @@ import { TasksRepository } from './tasks.repository';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
   constructor(private tasksRepository: TasksRepository) {}
+
+  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto);
+  }
 
   async getTaskById(id: string): Promise<Task> {
     const found = await this.tasksRepository.findOneBy({ id });
